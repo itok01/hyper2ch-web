477f3ea4455b53c9b8ac9b229069340f0bcae3c9
itok01 <28337009+itok01@users.noreply.github.com>
2020-09-28 15:26:24 +0900

    Add sample columns

---
+++ components/Column.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { createStyles, List, makeStyles } from "@material-ui/core"
+import Item from './Item'
+
+const useStyles = makeStyles((theme) => createStyles({
+  root: {
+    marginLeft: theme.spacing(0.5),
+    marginRight: theme.spacing(0.5),
+    width: theme.spacing(32),
+    height: '100%',
+    flexDirection: 'column',
+    display: 'inline-block',
+    overflowY: 'auto',
+  },
+}));
+
+const Column = () => {
+  const classes = useStyles();
+
+  return (
+    <List className={classes.root}>
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+    </List>
+  );
+}
+
+export default Column;
